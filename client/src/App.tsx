d88d975cc18a8bb73c635ab87bf187ebefc47003
Hemant Raj <coder.hemantraj05@gmail.com>
2025-02-03 16:55:03 +0530

    Created user controller and created skeleton for frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import ThemeSelector from './components/ThemeSelector'
+
+function App() {
+
+  return (
+   <div>
+    <ThemeSelector />
+    <h1 className="text-3xl font-bold underline">Hello, world!</h1>
+   </div>
+  )
+}
+
+export default App
